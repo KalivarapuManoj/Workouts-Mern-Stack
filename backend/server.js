d08fe474b17bd53f254ed12e0d2a396ddb006842
Manoj <manojkalivarapu390@gmail.com>
2025-03-06 11:18:44 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,32 @@
+require('dotenv').config();
+
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const workoutRoutes = require('./routes/workout');
+const userRoutes = require('./routes/user');
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+
+app.use((req,res,next) => {
+    console.log(req.path,req.method);
+    next();
+});
+
+
+app.use('/api/workouts',workoutRoutes);
+app.use('/api/user',userRoutes);
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        app.listen(process.env.PORT, () => {
+            console.log("connected to db and listening on port ",process.env.PORT);
+        });
+    }).catch((error) => {
+        console.log(error);
+    })
+
